706a20ca7d8402dcc7c72ba3f3026b72b3ea9513
Glacy <glacy.kelly3@gmail.com>
2021-07-21 09:27:35 -0300

    add createListener()

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 class Calculadora {
     constructor(){
         this.visor = document.getElementById('resultado')
+        this.createListener()
+    }
+    createListener(){
+        //Botões dos números
+        document.getElementById("num1").addEventListener("click", () => this.numeroVisor('1'))
+        document.getElementById("num2").addEventListener("click", () => this.numeroVisor('2'))
+        document.getElementById("num3").addEventListener("click", () => this.numeroVisor('3'))
+        document.getElementById("num4").addEventListener("click", () => this.numeroVisor('4'))
+        document.getElementById("num5").addEventListener("click", () => this.numeroVisor('5'))
+        document.getElementById("num6").addEventListener("click", () => this.numeroVisor('6'))
+        document.getElementById("num7").addEventListener("click", () => this.numeroVisor('7'))
+        document.getElementById("num8").addEventListener("click", () => this.numeroVisor('8'))
+        document.getElementById("num9").addEventListener("click", () => this.numeroVisor('9'))
+        document.getElementById("num0").addEventListener("click", () => this.numeroVisor('0'))
+
+        //Operações
+        document.getElementById("somar").addEventListener("click", () => this.numeroVisor('+'))
+        document.getElementById("subtrair").addEventListener("click", () => this.numeroVisor('-'))
+        document.getElementById("multiplicar").addEventListener("click", () => this.numeroVisor('*'))
+        document.getElementById("dividir").addEventListener("click", () => this.numeroVisor('/'))
+
+        //Função de Limpar
+        document.getElementById("limpar").addEventListener("click", () => this.limpar())
+
+        //Função de Calcular
+        document.getElementById("igual").addEventListener("click", () => this.calcular())
     }
 
     numeroVisor(numeroCalculadora){
@@ -24,26 +50,5 @@ class Calculadora {
 //Criando o novo objeto
 let calculadora = new Calculadora();
 
-//Botões dos números
-document.getElementById("num1").addEventListener("click", () => calculadora.numeroVisor('1'))
-document.getElementById("num2").addEventListener("click", () => calculadora.numeroVisor('2'))
-document.getElementById("num3").addEventListener("click", () => calculadora.numeroVisor('3'))
-document.getElementById("num4").addEventListener("click", () => calculadora.numeroVisor('4'))
-document.getElementById("num5").addEventListener("click", () => calculadora.numeroVisor('5'))
-document.getElementById("num6").addEventListener("click", () => calculadora.numeroVisor('6'))
-document.getElementById("num7").addEventListener("click", () => calculadora.numeroVisor('7'))
-document.getElementById("num8").addEventListener("click", () => calculadora.numeroVisor('8'))
-document.getElementById("num9").addEventListener("click", () => calculadora.numeroVisor('9'))
-document.getElementById("num0").addEventListener("click", () => calculadora.numeroVisor('0'))
 
-//Operações
-document.getElementById("somar").addEventListener("click", () => calculadora.numeroVisor('+'))
-document.getElementById("subtrair").addEventListener("click", () => calculadora.numeroVisor('-'))
-document.getElementById("multiplicar").addEventListener("click", () => calculadora.numeroVisor('*'))
-document.getElementById("dividir").addEventListener("click", () => calculadora.numeroVisor('/'))
 
-//Função de Limpar
-document.getElementById("limpar").addEventListener("click", () => calculadora.limpar())
-
-//Função de Calcular
-document.getElementById("igual").addEventListener("click", () => calculadora.calcular())
